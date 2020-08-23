890a84eb2ad730c5cb357aedb9ea67806f5eea57
lj2152 <65250556+lj2152@users.noreply.github.com>
2020-08-22 23:15:00 -0400

    Reformat Date - Leetcode 1507 (Easy)
    https://leetcode.com/problems/reformat-date/

---
+++ ReformatDate.cs
@@ -0,0 +1,33 @@
+//https://leetcode.com/problems/reformat-date/
+
+    public string ReformatDate(string date) {
+        
+       var splitDate = date.Split(' ');
+       var reformatted = new StringBuilder();
+        
+        var monthDict = new Dictionary<string,string>(){ {"Jan","01"},{"Feb","02"},{"Mar","03"},{"Apr","04"},{"May","05"},{"Jun","06"},{"Jul","07"},{"Aug","08"},{"Sep","09"},{"Oct","10"},{"Nov","11"},{"Dec","12"}};
+        
+		//year
+        var year = splitDate[2];
+        reformatted.Append(year);
+        reformatted.Append("-");
+		
+		//month
+        var monthString = splitDate[1];
+        reformatted.Append(monthDict[monthString]);
+        reformatted.Append("-");
+        
+		//day
+        var dayString = splitDate[0];
+        var lengthOfDayWithoutSuffix = dayString.Length-2;     
+		
+		reformatted.Append(lengthOfDayWithoutSuffix < 2 ? "0" : "");
+        
+        for(int i = 0; i< lengthOfDayWithoutSuffix;i++)
+        {
+             reformatted.Append(dayString[i]);
+        }       
+        
+        return reformatted.ToString();      
+        
+    }
