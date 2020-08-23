18bf58588912888861cb1eb0a59fad582fc4ce5f
lj2152 <65250556+lj2152@users.noreply.github.com>
2020-08-22 23:45:27 -0400

    Text Justification - Leetcode 68 (Hard)

--- FullTextJustification.cs
+++ FullTextJustification.cs
@@ -1,86 +1,85 @@
 //https://leetcode.com/problems/text-justification/ 
 	
-	public IList<string> FullJustify(string[] words, int maxWidth) {
-        
-         var resultList = new List<string>();
-            int totalNumberofWords = words.Length;
-            int currentWordIndex = 0;
+public IList<string> FullJustify(string[] words, int maxWidth) 
+{        
+    var resultList = new List<string>();
+    int totalNumberofWords = words.Length;
+    int currentWordIndex = 0;
 
-            while (currentWordIndex < totalNumberofWords)
-            {
-                int totalCharsOfCurrentLine = words[currentWordIndex].Length;
-                int nextWordIndex = currentWordIndex + 1;
+    while (currentWordIndex < totalNumberofWords)
+    {
+	int totalCharsOfCurrentLine = words[currentWordIndex].Length;
+	int nextWordIndex = currentWordIndex + 1;
 
-                //loop through until we reach the end of the words list...or see break below...
-                while (nextWordIndex < totalNumberofWords)
-                {
-                    int lengthOfNextWord = words[nextWordIndex].Length;
-					
-					int lineLengthWithNextWord = totalCharsOfCurrentLine + 1 + lengthOfNextWord;
-                    
-                    //if we exceed the maxWidth, then break out of this while loop..
-                    if (lineLengthWithNextWord > maxWidth)
-                    {
-                        break;
-                    }
-		    //include the length of the next word into the totalCharsOfCurrentWord + the space.
-		    else
-		    {
-			totalCharsOfCurrentLine = lineLengthWithNextWord;
-		    }                    
-                    nextWordIndex++;
-                }
+	//loop through until we reach the end of the words list...or see break below...
+	while (nextWordIndex < totalNumberofWords)
+	{
+	    int lengthOfNextWord = words[nextWordIndex].Length;					
+	    int lineLengthWithNextWord = totalCharsOfCurrentLine + 1 + lengthOfNextWord;
 
-                int numberOfGapsBetweenWords = nextWordIndex - currentWordIndex - 1;
-                var sb = new StringBuilder();
+	    //if we exceed the maxWidth, then break out of this while loop..
+	    if (lineLengthWithNextWord > maxWidth)
+	    {
+		break;
+	    }			
+	    //include the length of the next word into the totalCharsOfCurrentWord + the space.
+	    else
+	    {
+		totalCharsOfCurrentLine = lineLengthWithNextWord;
+	    }                    
+	    nextWordIndex++;
+	}
 
-                //if last word...
-                if (nextWordIndex == totalNumberofWords || numberOfGapsBetweenWords == 0)
-                {
-                    for (int i = currentWordIndex; i < nextWordIndex; i++)
-                    {
-                        sb.Append(words[i]);
-                        sb.Append(' ');
-                    }
+	int numberOfGapsBetweenWords = nextWordIndex - currentWordIndex - 1;
+	var sb = new StringBuilder();
 
-                    //remove last space...
-                    sb.Length--; 
-                    while (sb.Length < maxWidth)
-                    {
-                        sb.Append(' ');
-                    }
-                }
-                else
-                {
-                    int emptySpacesToBeFilled = maxWidth - totalCharsOfCurrentLine;
-                    int equalSpacesThatCanBeFilled = emptySpacesToBeFilled / numberOfGapsBetweenWords; //gives equal number spaces for each gap....
-                    int remainingSpacesToBeFilled = emptySpacesToBeFilled % numberOfGapsBetweenWords; //remainder gives us the rest that's left...
-                    
-                    int lastWordIdxOfCurrentLine = nextWordIndex - 1;
+	//if last word...
+	if (nextWordIndex == totalNumberofWords || numberOfGapsBetweenWords == 0)
+	{
+	    for (int i = currentWordIndex; i < nextWordIndex; i++)
+	    {
+		sb.Append(words[i]);
+		sb.Append(' ');
+	    }
 
-                    for (int i = currentWordIndex; i < lastWordIdxOfCurrentLine; i++)
-                    {
-                        sb.Append(words[i]);
-                        sb.Append(' ');
+	    //remove last space...
+	    sb.Length--; 
+	    while (sb.Length < maxWidth)
+	    {
+		sb.Append(' ');
+	    }
+	}
+	else
+	{
+	    int emptySpacesToBeFilled = maxWidth - totalCharsOfCurrentLine;
+	    int equalSpacesThatCanBeFilled = emptySpacesToBeFilled / numberOfGapsBetweenWords; //gives equal number spaces for each gap....
+	    int remainingSpacesToBeFilled = emptySpacesToBeFilled % numberOfGapsBetweenWords; //remainder gives us the rest that's left...
 
-                        for (int j = 0; j < equalSpacesThatCanBeFilled; j++)
-                        {
-                            sb.Append(' ');
-                        }
+	    int lastWordIdxOfCurrentLine = nextWordIndex - 1;
 
-                        if(remainingSpacesToBeFilled > 0)
-                        {
-                            sb.Append(' ');
-                            remainingSpacesToBeFilled--;
-                        }
-                    }
+	    for (int i = currentWordIndex; i < lastWordIdxOfCurrentLine; i++)
+	    {
+		sb.Append(words[i]);
+		sb.Append(' ');
 
-                    sb.Append(words[lastWordIdxOfCurrentLine]);
-                }
+		for (int j = 0; j < equalSpacesThatCanBeFilled; j++)
+		{
+		    sb.Append(' ');
+		}
 
-                resultList.Add(sb.ToString());
-                currentWordIndex = nextWordIndex;
-            }
+		if(remainingSpacesToBeFilled > 0)
+		{
+		    sb.Append(' ');
+		    remainingSpacesToBeFilled--;
+		}
+	    }
 
-            return resultList;
-        }
+	    sb.Append(words[lastWordIdxOfCurrentLine]);
+	}
+
+	resultList.Add(sb.ToString());
+	currentWordIndex = nextWordIndex;
+    }
+
+    return resultList;
+}
