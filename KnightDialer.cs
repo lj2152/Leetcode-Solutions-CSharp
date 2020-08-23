97cb551120c17cce5247f41a2f08785acf67517d
lj2152 <65250556+lj2152@users.noreply.github.com>
2020-08-22 23:33:53 -0400

    Knight Dialer - Leetcode 935 (Medium)

--- KnightDialer.cs
+++ KnightDialer.cs
@@ -3,15 +3,18 @@
 
   public int KnightDialer(int N) {
   
-			// 0 1 2 3 4 5 6 7 8 9
-  
+	    // 0 1 2 3 4 5 6 7 8 9  (Our indexes for the "fromPath" below)
+	    // 4 & 6 below are paths from which the knight can get to index 0
+	    // 6 & 8 below are paths from which the knight can get to index 1
+	    // 7 & 9 below are paths from which the knight can get to index 2
+	    // We can go through this process to build our "fromPath" below...
   
            //all possible "from" paths for each digit (index)...
             var fromPath = new int[][] { new int[]{ 4, 6 }, new int[] { 6, 8 }, new int[] { 7, 9 }, new int[] { 4, 8 },
                                 new int[]{ 3, 9, 0 }, new int[]{ }, new int[]{ 1, 7, 0 }, new int[]{ 2, 6 }, new int[]{ 1, 3 }, new int[]{ 2, 4 } };
             
-			//this has to be long instead of int due to this number being this large
-			long MOD = 1000000007;
+	    //this has to be long instead of int due to this number being this large, we can cast it back to an int at the end...
+	    long MOD = 1000000007;
 
             var steps = N + 1; //include step 0 so N+1
             var possibleNumbers = 10; //0 through 9 in the keypad
